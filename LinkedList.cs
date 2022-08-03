d0b9b5c8540285d927c322c22ef9b4a298fed69f
Hema <hemasahu019@gmail.com>
2022-08-03 17:26:20 +0530

    Added UC1 Code to create simple LinkedlIst

---
+++ LinkedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureUsingGenerics
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} is added", data);
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = newNode;
+                Console.WriteLine("{0} is added", data);
+            }
+        }
+
+        public Node GetLastNode()
+        {   
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,30 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            int option;
             Console.WriteLine("Welcome to the Data Structure Problems using C# Generics");
+
+            do {
+                
+                Console.WriteLine("Please select the Options from Below: ");
+                Console.WriteLine("1.Create a simple Linked List of 56, 30 and 70");
+                Console.Write("2.Exit  ");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        LinkedList linkedListObj = new LinkedList();
+                        linkedListObj.AddLast(56);
+                        linkedListObj.AddLast(30);
+                        linkedListObj.AddLast(70);
+                        Console.WriteLine();
+                        break;
+                }
+            }while (option !=2);
+            
+
          }
     }
 }
